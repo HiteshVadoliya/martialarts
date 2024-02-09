c82944fae6e6fecf7a28333d57eca6020e0169fa
jack <hmvadoliya.iipl2013@gmail.com>
2024-02-09 20:09:58 +0530

    upload on live

--- details/martialarts (1).sql
+++ details/martialarts (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 28, 2024 at 05:53 AM
+-- Generation Time: Feb 09, 2024 at 03:27 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -100,41 +100,6 @@ INSERT INTO `location` (`location_id`, `location_title`, `status`, `isDelete`, `
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Phrases`
---
-
-CREATE TABLE `Phrases` (
-  `Phrase_id` int(11) NOT NULL,
-  `Phrase` varchar(30) NOT NULL,
-  `Good` tinyint(1) NOT NULL,
-  `Weight` int(11) NOT NULL,
-  `Camp_pid` varchar(10) NOT NULL COMMENT 'ALL indicates all campaigns',
-  `Client` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `Phrases`
---
-
-INSERT INTO `Phrases` (`Phrase_id`, `Phrase`, `Good`, `Weight`, `Camp_pid`, `Client`) VALUES
-(1, 'person charged 12/17/2023', 1, 50, 'ATAExcelle', 0),
-(2, 'Thank You', 1, 32, 'ALL', 0),
-(3, 'gotta', 0, 10, 'ALL', 0),
-(4, 'Thank You', 1, 8, 'BE', 0),
-(8, 'Airport', 0, 4, 'BE', 0),
-(19, 'repeat', 0, 42, '[ALL]', 0),
-(21, 'Airport', 0, 3, 'EMPIRE', 0),
-(22, 'okay', 1, 8, '[ALL]', 0),
-(23, 'why', 1, 10, '[ALL]', 0),
-(24, 'hello', 1, 5, '[ALL]', 0),
-(25, 'how', 1, 5, '[ALL]', 0),
-(26, 'you', 0, 6, '[ALL]', 0),
-(27, 'like', 0, 2, '[ALL]', 0),
-(28, '1', 1, 0, 'EmpireDefF', 0);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `rank`
 --
 
@@ -168,9 +133,14 @@ INSERT INTO `rank` (`rank_id`, `rank_title`, `status`, `isDelete`, `created_at`,
 
 CREATE TABLE `schedule` (
   `schedule_id` int(10) UNSIGNED NOT NULL,
-  `schedule_title` varchar(255) NOT NULL,
+  `class_pid` int(11) DEFAULT NULL,
   `school_pid` int(11) NOT NULL,
-  `schedule_time` datetime DEFAULT NULL,
+  `instructor_pid` int(11) NOT NULL,
+  `weekday_pid` int(11) DEFAULT NULL,
+  `day_time` time DEFAULT NULL,
+  `effective_date_from` date DEFAULT NULL,
+  `effective_date_to` date DEFAULT NULL,
+  `effective_date` varchar(150) NOT NULL,
   `week` int(11) NOT NULL,
   `day` varchar(50) NOT NULL,
   `day_number` int(11) NOT NULL,
@@ -184,18 +154,54 @@ CREATE TABLE `schedule` (
 -- Dumping data for table `schedule`
 --
 
-INSERT INTO `schedule` (`schedule_id`, `schedule_title`, `school_pid`, `schedule_time`, `week`, `day`, `day_number`, `status`, `isDelete`, `created_at`, `updated_at`) VALUES
-(2, 'Legal Instructor', 1, '2024-01-29 10:56:00', 5, 'Monday', 1, 1, 0, '2024-01-27 09:57:03', '2024-01-27 05:36:33'),
-(3, 'Home School', 1, '2024-01-29 11:59:00', 5, 'Monday', 1, 1, 0, '2024-01-27 09:57:34', '2024-01-27 05:36:33'),
-(4, 'Tiger Cub', 1, '2024-01-29 11:43:00', 5, 'Monday', 1, 1, 0, '2024-01-27 10:02:01', '2024-01-27 05:37:25'),
-(5, 'Cubs', 3, '2024-01-29 18:14:00', 5, 'Monday', 1, 1, 0, '2024-01-27 10:02:36', '2024-01-27 05:37:27'),
-(6, 'Beginner Sparring', 1, '2024-01-30 10:02:00', 5, 'Tuesday', 2, 1, 0, '2024-01-27 10:03:19', '2024-01-27 05:36:33'),
-(7, 'XMA', 1, '2024-01-30 05:03:00', 5, 'Tuesday', 2, 1, 0, '2024-01-27 10:03:43', '2024-01-27 05:36:33'),
-(8, 'Homeschool', 1, '2024-01-31 06:11:00', 5, 'Wednesday', 3, 1, 0, '2024-01-27 10:04:36', '2024-01-27 05:36:33'),
-(9, 'Tiger Cub', 2, '2024-01-31 10:04:00', 5, 'Wednesday', 3, 1, 0, '2024-01-27 10:06:05', '2024-01-27 05:36:40'),
-(10, 'instructor class', 3, '2024-02-01 04:11:00', 5, 'Thursday', 4, 1, 0, '2024-01-27 10:08:19', '2024-01-27 05:36:49'),
-(11, 'test44', 2, '2024-02-09 11:08:00', 6, 'Friday', 5, 1, 0, '2024-01-27 11:09:10', NULL),
-(12, 'test 4', 3, '2024-02-10 11:09:00', 6, 'Saturday', 6, 1, 0, '2024-01-27 11:09:29', NULL);
+INSERT INTO `schedule` (`schedule_id`, `class_pid`, `school_pid`, `instructor_pid`, `weekday_pid`, `day_time`, `effective_date_from`, `effective_date_to`, `effective_date`, `week`, `day`, `day_number`, `status`, `isDelete`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 2, 1, '10:00:00', '2024-02-12', '2024-02-17', '2024-02-12 - 2024-02-17', 0, '', 0, 1, 0, '2024-02-08 19:54:00', NULL),
+(2, 3, 1, 8091, 1, '11:00:00', '2024-02-12', '2024-02-17', '2024-02-12 - 2024-02-17', 0, '', 0, 1, 0, '2024-02-08 19:54:51', NULL),
+(3, 2, 1, 8092, 1, '12:10:00', '2024-02-12', '2024-02-17', '2024-02-12 - 2024-02-17', 0, '', 0, 1, 0, '2024-02-08 19:55:23', NULL),
+(4, 1, 2, 2, 1, '14:30:00', '2024-02-12', '2024-02-17', '2024-02-12 - 2024-02-17', 0, '', 0, 1, 0, '2024-02-08 19:56:01', NULL),
+(5, 3, 2, 8092, 1, '15:30:00', '2024-02-12', '2024-02-17', '2024-02-12 - 2024-02-17', 0, '', 0, 1, 0, '2024-02-08 19:56:32', NULL),
+(6, 2, 3, 8091, 1, '14:10:00', '2024-02-12', '2024-02-17', '2024-02-12 - 2024-02-17', 0, '', 0, 1, 0, '2024-02-08 19:57:02', NULL),
+(7, 1, 1, 8093, 2, '21:35:00', '2024-02-12', '2024-02-17', '2024-02-12 - 2024-02-17', 0, '', 0, 1, 0, '2024-02-08 19:57:58', NULL),
+(8, 6, 1, 8093, 2, '15:10:00', '2024-02-12', '2024-02-17', '2024-02-12 - 2024-02-17', 0, '', 0, 1, 0, '2024-02-08 19:58:33', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedule_bk`
+--
+
+CREATE TABLE `schedule_bk` (
+  `schedule_id` int(10) UNSIGNED NOT NULL,
+  `schedule_title` varchar(255) NOT NULL,
+  `school_pid` int(11) NOT NULL,
+  `schedule_time` datetime DEFAULT NULL,
+  `week` int(11) NOT NULL,
+  `day` varchar(50) NOT NULL,
+  `day_number` int(11) NOT NULL,
+  `assign_pid` int(11) DEFAULT NULL,
+  `assign_as` enum('Lead','Assistance') DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1,
+  `isDelete` tinyint(4) NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `schedule_bk`
+--
+
+INSERT INTO `schedule_bk` (`schedule_id`, `schedule_title`, `school_pid`, `schedule_time`, `week`, `day`, `day_number`, `assign_pid`, `assign_as`, `status`, `isDelete`, `created_at`, `updated_at`) VALUES
+(2, 'Legal Instructor', 1, '2024-01-29 10:56:00', 5, 'Monday', 1, NULL, NULL, 1, 0, '2024-01-27 09:57:03', '2024-01-27 05:36:33'),
+(3, 'Home School', 1, '2024-01-29 11:59:00', 5, 'Monday', 1, NULL, NULL, 1, 0, '2024-01-27 09:57:34', '2024-01-27 05:36:33'),
+(4, 'Tiger Cub', 1, '2024-01-29 11:43:00', 5, 'Monday', 1, NULL, NULL, 1, 0, '2024-01-27 10:02:01', '2024-01-27 05:37:25'),
+(5, 'Cubs', 3, '2024-01-29 18:14:00', 5, 'Monday', 1, NULL, NULL, 0, 0, '2024-01-27 10:02:36', '2024-01-27 05:37:27'),
+(6, 'Beginner Sparring', 1, '2024-01-30 10:02:00', 5, 'Tuesday', 2, NULL, NULL, 1, 0, '2024-01-27 10:03:19', '2024-01-27 05:36:33'),
+(7, 'XMA', 1, '2024-01-30 05:03:00', 5, 'Tuesday', 2, NULL, NULL, 1, 0, '2024-01-27 10:03:43', '2024-01-27 05:36:33'),
+(8, 'Homeschool', 1, '2024-01-31 06:11:00', 5, 'Wednesday', 3, NULL, NULL, 1, 0, '2024-01-27 10:04:36', '2024-01-27 05:36:33'),
+(9, 'Tiger Cub', 2, '2024-01-31 10:04:00', 5, 'Wednesday', 3, NULL, NULL, 1, 0, '2024-01-27 10:06:05', '2024-01-27 05:36:40'),
+(10, 'instructor class', 3, '2024-02-01 04:11:00', 5, 'Thursday', 4, NULL, NULL, 1, 0, '2024-01-27 10:08:19', '2024-01-27 05:36:49'),
+(11, 'test44', 2, '2024-02-09 11:08:00', 6, 'Friday', 5, NULL, NULL, 1, 0, '2024-01-27 11:09:10', NULL),
+(12, 'test 4', 3, '2024-02-10 11:09:00', 6, 'Saturday', 6, NULL, NULL, 1, 0, '2024-01-27 11:09:29', NULL);
 
 -- --------------------------------------------------------
 
@@ -355,23 +361,13 @@ CREATE TABLE `tbl_user` (
 
 INSERT INTO `tbl_user` (`user_id`, `fname`, `lname`, `username`, `password`, `email`, `b_date`, `level_pid`, `rank_pid`, `hr_rate`, `class_pid`, `location_pid`, `phone`, `image`, `is_active`, `status`, `isDelete`, `created_at`, `updated_at`) VALUES
 (1, 'admin', '', 'admin', '$2y$10$E4BY2GTa2hBB/ZW0ytTRXOwL6XrqVZnprg9HfgLvOxmlohVMl6Etu', 'admin@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, '1231230001', NULL, 1, 1, 0, '2024-01-21 09:20:40', NULL),
-(2, 'Instructor', '', 'instructor', '$2y$10$KQOKdnsOe0ZAMorNxMntk.QmBX5248RgEc0zGEW.aa0cIbZAOGdG2', 'instructor@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, '1231230002', '', 1, 1, 0, '2024-01-21 09:20:40', NULL),
-(3, 'Assistants', '', 'assistants', '$2y$10$bFD6b78OVQxF6qy0Rx66v.xOqLAd7EqRt.LjOoGpJrrf2kpJ3WFZC', 'assistants@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, '1231230003', NULL, 1, 0, 0, '2024-01-21 09:20:40', NULL),
+(2, 'ekon ins', '', 'instructor', '$2y$10$KQOKdnsOe0ZAMorNxMntk.QmBX5248RgEc0zGEW.aa0cIbZAOGdG2', 'instructor@gmail.com', NULL, 1, 1, 10.00, '2', '2', '1231230002', '', 1, 1, 0, '2024-01-21 09:20:40', '2024-02-08 14:23:18'),
+(3, 'Assistants', '', 'assistants', '$2y$10$bFD6b78OVQxF6qy0Rx66v.xOqLAd7EqRt.LjOoGpJrrf2kpJ3WFZC', 'assistants@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, '1231230003', NULL, 1, 1, 0, '2024-01-21 09:20:40', NULL),
 (4, 'Students', '', 'students', '$2y$10$mX1G5dLstYqNl8GA1X8hXefVGpQar6PC68BYOcbb7FWhsUTBMJBGe', 'student@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, '1231320004', NULL, 1, 0, 0, '2024-01-21 09:20:40', NULL),
-(5, 'ins2', '', 'ins', '$2y$10$E4BY2GTa2hBB/ZW0ytTRXOwL6XrqVZnprg9HfgLvOxmlohVMl6Etu', 'ins@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, '1231230005', NULL, 1, 0, 0, '2024-01-21 09:20:40', NULL),
-(8078, 'Assistants', '', 'assistants', '$2y$10$bFD6b78OVQxF6qy0Rx66v.xOqLAd7EqRt.LjOoGpJrrf2kpJ3WFZC', 'assistants@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, '1231230003', NULL, 1, 0, 0, '2024-01-21 09:20:40', NULL),
-(8079, 'Students', '', 'students', '$2y$10$mX1G5dLstYqNl8GA1X8hXefVGpQar6PC68BYOcbb7FWhsUTBMJBGe', 'student@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, '1231320004', NULL, 1, 0, 0, '2024-01-21 09:20:40', NULL),
-(8080, 'ins2', '', 'ins', '$2y$10$E4BY2GTa2hBB/ZW0ytTRXOwL6XrqVZnprg9HfgLvOxmlohVMl6Etu', 'ins@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, '1231230005', NULL, 1, 0, 0, '2024-01-21 09:20:40', NULL),
-(8081, 'Hitesh', 'Vadoliya', '', '$2y$10$62z7CJJtbqeCUhnWV4ScyubB4D.jp2HUcrgjIqLM3tZ0LxtOJ7Rq6', '1@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 0, '2024-01-21 12:27:20', NULL),
-(8082, 'jack', 'k', '', '$2y$10$7GHp3ZZNBAWFGGUkAHmexukNVNOJanDJXFOKj0D6/19KEAIfrfEsq', 'k@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 0, '2024-01-21 12:29:13', '2024-01-21 07:21:00'),
-(8083, 'a44', 'b', '', '$2y$10$TsCen63H8wmmdEWuV6GBWucAB2EsPmgoMmuSfFxt4t1H9fiwGwI8u', 'c@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, '2024-01-21 12:36:25', NULL),
-(8084, 'Assistants', '', 'assistants', '$2y$10$bFD6b78OVQxF6qy0Rx66v.xOqLAd7EqRt.LjOoGpJrrf2kpJ3WFZC', 'assistants@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, '1231230003', NULL, 1, 0, 0, '2024-01-21 09:20:40', NULL),
-(8085, 'Students', '', 'students', '$2y$10$mX1G5dLstYqNl8GA1X8hXefVGpQar6PC68BYOcbb7FWhsUTBMJBGe', 'student@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, '1231320004', NULL, 1, 0, 0, '2024-01-21 09:20:40', NULL),
-(8086, 'ins2', '', 'ins', '$2y$10$E4BY2GTa2hBB/ZW0ytTRXOwL6XrqVZnprg9HfgLvOxmlohVMl6Etu', 'ins@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, '1231230005', NULL, 1, 0, 0, '2024-01-21 09:20:40', NULL),
-(8087, 'Hitesh', 'Vadoliya', '', '$2y$10$62z7CJJtbqeCUhnWV4ScyubB4D.jp2HUcrgjIqLM3tZ0LxtOJ7Rq6', '1@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 0, '2024-01-21 12:27:20', NULL),
-(8088, 'jack', 'k', '', '$2y$10$7GHp3ZZNBAWFGGUkAHmexukNVNOJanDJXFOKj0D6/19KEAIfrfEsq', 'k@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, '2024-01-21 12:29:13', '2024-01-21 07:21:00'),
-(8089, 'a44', 'b', '', '$2y$10$TsCen63H8wmmdEWuV6GBWucAB2EsPmgoMmuSfFxt4t1H9fiwGwI8u', 'c@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, 0, 1, '2024-01-21 12:36:25', NULL),
-(8090, 'Hitesh Vadoliya', '', '', '$2y$10$XnVIEOyHTU6PbKeL3uI6pe4O4xgJUVT3eueu8fptJx3bstA3iVKXm', '11@gmail.com', NULL, 3, 4, 25.00, '2,3,6', '2,3', '8866005130', NULL, 0, 1, 0, '2024-01-22 18:36:27', '2024-01-27 01:49:34');
+(5, 'Smith Ins', '', 'ins', '$2y$10$E4BY2GTa2hBB/ZW0ytTRXOwL6XrqVZnprg9HfgLvOxmlohVMl6Etu', 'smith@gmail.com', NULL, 1, 1, 10.00, '1,2,3,6', '1,2', '1231230005', NULL, 1, 0, 0, '2024-01-21 09:20:40', '2024-02-08 14:17:56'),
+(8091, 'Jack ins', '', '', '$2y$10$gkjvOhVLjuV4g7lR7.GaV.6dx9XpsoBXRwSoLsM7ReHCTwt72iney', 'jack@gmail.com', NULL, 2, 2, 1.00, '1,2,4,6', '1', '123', NULL, 0, 1, 0, '2024-02-06 21:27:00', '2024-02-08 14:18:07'),
+(8092, 'will ins', '', '', '$2y$10$jQJznHHrBVOWWWAaDsVi.edFQI1tLrph7iPQZnCoQ0ahNaseRsKX6', 'will@gmail.com', NULL, 2, 3, 2.00, '2,3', '3', '9898989898', NULL, 0, 1, 0, '2024-02-08 19:48:46', NULL),
+(8093, 'ruth ins', '', '', '$2y$10$xmxsugdhXxWC1YbR3LwTGeau0w4XhDOtney/0n7isCmqsNbGSglNG', 'ruth@gmail.com', NULL, 4, 4, 10.00, '1,2,6', '1', '7897897899', NULL, 0, 1, 0, '2024-02-08 19:49:25', NULL);
 
 -- --------------------------------------------------------
 
@@ -400,9 +396,40 @@ INSERT INTO `tbl_user_role` (`user_role_id`, `user_pid`, `role_pid`, `created_at
 (3, 3, 3, '2024-01-21 12:32:08', NULL, '2023-10-16 00:00:00', '', 1),
 (4, 4, 4, '2024-01-21 12:32:08', NULL, '2023-11-07 11:29:50', '', 1),
 (8, 5, 2, '2024-01-21 12:32:08', NULL, '2023-11-07 11:29:50', '', 1),
-(9, 5, 4, '2024-01-21 12:32:08', NULL, '2023-11-07 11:29:50', '', 1),
+(9, 5, 2, '2024-01-21 12:32:08', NULL, '2023-11-07 11:29:50', '', 1),
 (10, 8083, 2, '2024-01-21 12:36:25', NULL, '0000-00-00 00:00:00', '', 0),
-(14, 8090, 2, '2024-01-21 12:36:25', NULL, '0000-00-00 00:00:00', '', 0);
+(14, 8090, 2, '2024-01-21 12:36:25', NULL, '0000-00-00 00:00:00', '', 0),
+(15, 8091, 2, '2024-02-06 21:27:00', NULL, '0000-00-00 00:00:00', '', 0),
+(16, 8092, 2, '2024-02-08 19:48:46', NULL, '0000-00-00 00:00:00', '', 0),
+(17, 8093, 2, '2024-02-08 19:49:25', NULL, '0000-00-00 00:00:00', '', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `weekdays`
+--
+
+CREATE TABLE `weekdays` (
+  `weekdays_id` int(10) UNSIGNED NOT NULL,
+  `week_title` varchar(155) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1,
+  `isDelete` tinyint(4) NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `weekdays`
+--
+
+INSERT INTO `weekdays` (`weekdays_id`, `week_title`, `status`, `isDelete`, `created_at`, `updated_at`) VALUES
+(1, 'Monday', 1, 0, '2024-02-06 21:29:32', NULL),
+(2, 'Tuesday', 1, 0, '2024-02-06 21:29:32', NULL),
+(3, 'Wednesday', 1, 0, '2024-02-06 21:29:50', NULL),
+(4, 'Thursday', 1, 0, '2024-02-06 21:29:50', NULL),
+(5, 'Friday', 1, 0, '2024-02-06 21:29:58', NULL),
+(6, 'Saturday', 1, 0, '2024-02-06 21:29:58', NULL),
+(7, 'Sunday', 1, 0, '2024-02-06 21:30:04', NULL);
 
 --
 -- Indexes for dumped tables
@@ -427,12 +454,6 @@ ALTER TABLE `location`
   ADD PRIMARY KEY (`location_id`);
 
 --
--- Indexes for table `Phrases`
---
-ALTER TABLE `Phrases`
-  ADD PRIMARY KEY (`Phrase_id`);
-
---
 -- Indexes for table `rank`
 --
 ALTER TABLE `rank`
@@ -442,6 +463,12 @@ ALTER TABLE `rank`
 -- Indexes for table `schedule`
 --
 ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`schedule_id`);
+
+--
+-- Indexes for table `schedule_bk`
+--
+ALTER TABLE `schedule_bk`
   ADD PRIMARY KEY (`schedule_id`);
 
 --
@@ -482,6 +509,12 @@ ALTER TABLE `tbl_user_role`
   ADD PRIMARY KEY (`user_role_id`);
 
 --
+-- Indexes for table `weekdays`
+--
+ALTER TABLE `weekdays`
+  ADD PRIMARY KEY (`weekdays_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -504,12 +537,6 @@ ALTER TABLE `location`
   MODIFY `location_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `Phrases`
---
-ALTER TABLE `Phrases`
-  MODIFY `Phrase_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
-
---
 -- AUTO_INCREMENT for table `rank`
 --
 ALTER TABLE `rank`
@@ -519,6 +546,12 @@ ALTER TABLE `rank`
 -- AUTO_INCREMENT for table `schedule`
 --
 ALTER TABLE `schedule`
+  MODIFY `schedule_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `schedule_bk`
+--
+ALTER TABLE `schedule_bk`
   MODIFY `schedule_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
@@ -549,13 +582,19 @@ ALTER TABLE `tbl_role`
 -- AUTO_INCREMENT for table `tbl_user`
 --
 ALTER TABLE `tbl_user`
-  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8091;
+  MODIFY `user_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8094;
 
 --
 -- AUTO_INCREMENT for table `tbl_user_role`
 --
 ALTER TABLE `tbl_user_role`
-  MODIFY `user_role_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `user_role_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `weekdays`
+--
+ALTER TABLE `weekdays`
+  MODIFY `weekdays_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
